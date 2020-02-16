db92d6307f24b9998402c866213d049682c26410
Gareth Tan <garethwtan@gmail.com>
2020-02-16 00:36:45 -0500

    [test] Refactor test naming and add test for lambda parameters
    Add a test for lambda parameters in preparation for refactoring parameter highlighting

---
+++ src/test/scala/org/intellij/plugins/dhall/annotator/HighlightAssert.scala
@@ -0,0 +1,21 @@
+package org.intellij.plugins.dhall
+package annotator
+
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.editor.colors.TextAttributesKey
+
+case class HighlightAssert(text: String,
+                           key: TextAttributesKey,
+                           severity: HighlightSeverity =
+                             HighlightSeverity.INFORMATION)
+
+object HighlightAssert {
+  def fromHighlightInfo(info: HighlightInfo): HighlightAssert = {
+    HighlightAssert(
+      text = info.getText,
+      key = info.forcedTextAttributesKey,
+      severity = info.getSeverity
+    )
+  }
+}
---
+++ src/test/scala/syntaxHighlightTestData/lambdaParameter.dhall
@@ -0,0 +1,1 @@
+\(nat : Natural) -> assert : n === (n + 0)
